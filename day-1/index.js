699df692713ddbb43f75904c8dd4d829a75340ff
Hrushi1230 <hrushikeshbehera1230@gmail.com>
2025-07-04 15:51:43 +0000

    Initial commit

---
+++ day-1/index.js
@@ -0,0 +1,21 @@
+//1a.
+//alert("Good Morning, World!");
+//1b.
+//alert("Hrushikesh Behera");
+//1c.
+// console.log(10 + 5);
+// //1d.
+// console.log(20 - 5);
+// //1e.
+// console.log(2 + 2 - 5);
+// //1f.       
+// document.body.innerHTML = "Hello World!";
+// //1g.
+// document.body.innerHTML = "Hrushikesh Behera";
+//1h.
+// let tshirt = 10;
+// let socks = 8;
+// let dinner = 20;
+// console.log("Total cost of the item is :" + (tshirt + socks + dinner) + "$");
+//1i.
+// document.body.innerHTML = "";
