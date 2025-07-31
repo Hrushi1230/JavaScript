70d49041e433c06839f4f6b7202bbc7bf5504145
Hrushi1230 <hrushikeshbehera1230@gmail.com>
2025-07-31 06:54:42 +0000

    We have work in select quantity and added cart

---
+++ day-11/javascript-amazon-project/scripts/amazon.js
@@ -0,0 +1,99 @@
+let allHtml = "";
+products.forEach((product) => {
+
+  allHtml += `<div class="product-container ">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            $${product.priceCents / 10}
+          </div>
+
+          <div class="product-quantity-container">
+            <select class ="js-quantity-select-${product.id}">
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary add-cart">
+            Add to Cart
+          </button>
+        </div>`
+
+})
+
+document.querySelector('.all-product').
+  innerHTML = allHtml;
+
+
+let cartProduct = [];
+let Totalcount = 0;
+
+document.querySelectorAll('.add-cart').forEach((cart, indx) => {
+  cart.addEventListener('click', () => {
+
+    addedToCart(cart);
+    const selectProduct = products[indx];
+    const selectQuantity = document.querySelector(`.js-quantity-select-${selectProduct.id}`);
+    const value = selectQuantity.value * 1;
+    Totalcount += value;
+    console.log(value);
+    const excitingProd = cartProduct.find(p => p.name === selectProduct.name);
+
+    if (excitingProd) {
+      excitingProd.count += value;
+
+    }
+    else {
+      cartProduct.push({
+        name: selectProduct.name,
+        count: value
+      });
+    }
+
+    document.querySelector('.cart-count').innerHTML = Totalcount;
+    console.log(cartProduct);
+
+  });
+});
+
+function addedToCart(cart) {
+  const addedToCart = cart.parentElement.querySelector('.added-to-cart');
+  addedToCart.style.opacity = '1';
+  setTimeout(() => {
+    addedToCart.style.opacity = '0';
+  }, 2000);
+}
+
+
+
